d459be90373e286acee36627099d78e2fd68e00a
Efrain.mesia <efrain.mesia@gmail.com>
2020-04-28 13:57:47 +0300

    added Comments & change the path of ISO

--- CreateVMWithPowerCLI.ps1
+++ CreateVMWithPowerCLI.ps1
@@ -8,7 +8,9 @@ $credentials=Get-Credential
 Connect-VIServer -Verbose:$true -Server 192.168.10.16 -Credential $credentials 
 
 #Get all iso's in datastore...................... Need to change the filter
-$isoWin = dir -Recurse -Path vmstores:\ -Include Win* | select name, Datastorefullpath
+#How to get the path for datastore? run - Get-Datastore NameOfDataStore | Select-Object Select-Object DatastoreBrowserPath -  you will find the dat
+$isoWin = dir -Recurse -Path vmstore:ha-datacenter\datastore1 -Include *iso | select name, Datastorefullpath
+
 
 #Create a new VM.
 #GuestID must be configured to an specific type of windows otherwise there will be a problem while you start the vm.
@@ -16,15 +18,16 @@ $isoWin = dir -Recurse -Path vmstores:\ -Include Win* | select name, Datastorefu
 New-VM -name $VMNewName -ResourcePool 192.168.10.16 -DiskGB 30 -MemoryGB 4 -NumCpu 2 -CD -NetworkName "VM Network" -DiskStorageFormat Thin -guestID windows9Server64Guest
 $VM = Get-VM $VMNewName
 
+
 #configure the CD and attach it to vm
 Get-CDDrive -vm $VMNewName | set-CDDrive -IsoPath $isoWin[2].DatastoreFullPath -Confirm:$false -StartConnected:$true
+
 
 #Change the network adapter to vmxnet3.
 $VM | Get-NetworkAdapter | Set-NetworkAdapter -type "vmxnet3" -NetworkName "VM Network" -StartConnected:$true -Confirm: $false
 
-#Reconfigure cpuHOTADD and memoryHotADD to enabled
 
-$spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+#Reconfigure cpuHOTADD and memoryHotADD to enabled
 $spec.memoryHotAddEnabled = $true
 $spec.cpuHotAddEnabled = $true
 $VM.ExtensionData.ReconfigVM_Task($spec)
