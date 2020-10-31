8c56a67143f9e00ac0d60e9af9995d725072b163
Efrain.mesia <efrain.mesia@gmail.com>
2020-10-31 20:55:14 +0200

    esage

---
+++ Create_profiles_in_putty.ps1
@@ -0,0 +1,28 @@
+$ComPorts_OK = Get-WmiObject Win32_SerialPort |Where-Object {$_.Status -like "*OK*"} | Select-Object deviceID
+$ComPorts_notworking = Get-WmiObject Win32_SerialPort |Where-Object {$_.Status -like "*Fail*"} | Select-Object deviceID
+$regedit_path = "HKCU:\SOFTWARE\SimonTatham\PuTTY\Sessions\"
+$regedit_source = "HKCU:\SOFTWARE\SimonTatham\PuTTY\Sessions\Default%20Settings"
+
+try {
+    foreach ($port in $ComPorts_notworking)
+    {
+        $noworkingport = $regedit_path + $port
+        Remove-Item -Path $noworkingport
+    }
+
+    Foreach ( $port in $ComPorts_OK )
+    {
+        Test-Path $regedit_path
+        $registry = $regedit_path + $port.deviceID
+        if(-Not (Test-Path $registry))
+        {
+            Write-Host $port.deviceID
+            $registry = $regedit_path + $port.deviceID
+            Copy-Item -Path $regedit_source -Destination $registry
+            Set-ItemProperty -Path $registry -Name SerialLine -Value $port.deviceID
+        }
+    }
+}
+catch{
+    Write-Host "Something went wrong"
+}
