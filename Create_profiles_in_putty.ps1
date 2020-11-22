814699fe08dbc3b19071c949f167d8c79f3be003
Efrain.mesia <efrain.mesia@gmail.com>
2020-11-22 09:44:44 +0200

    Added if default settings registry is not found

--- Create_profiles_in_putty.ps1
+++ Create_profiles_in_putty.ps1
@@ -9,14 +9,17 @@ try {
         $noworkingport = $regedit_path + $port
         Remove-Item -Path $noworkingport
     }
-
+    
+    if(-Not(Test-Path $regedit_source))
+    {
+        regedit.exe /C '.\default_settings.reg'
+    }
     Foreach ( $port in $ComPorts_OK )
     {
-        Test-Path $regedit_path
         $registry = $regedit_path + $port.deviceID
         if(-Not (Test-Path $registry))
         {
-            Write-Host $port.deviceID
+            Write-Host "Setting the port: '$($port."deviceID")'"
             $registry = $regedit_path + $port.deviceID
             Copy-Item -Path $regedit_source -Destination $registry
             Set-ItemProperty -Path $registry -Name SerialLine -Value $port.deviceID
