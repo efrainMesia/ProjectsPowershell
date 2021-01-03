d9c2fa618dcbb9c20600bbfb938010b6b69332b0
efrainMesia <64331443+efrainMesia@users.noreply.github.com>
2021-01-03 14:26:15 +0200

    Script is working with the all type of COM Ports

--- Create_profiles_in_putty.ps1
+++ Create_profiles_in_putty.ps1
@@ -1,31 +1,47 @@
-$ComPorts_OK = Get-WmiObject Win32_SerialPort |Where-Object {$_.Status -like "*OK*"} | Select-Object deviceID
-$ComPorts_notworking = Get-WmiObject Win32_SerialPort |Where-Object {$_.Status -like "*Fail*"} | Select-Object deviceID
+$ComPorts_OK = Get-WmiObject -query "SELECT * FROM Win32_PnPEntity" | ForEach-Object {if (($_.Name -match "COM\d+") -and ($_.Name -notlike "*Intel(R)*") -and ($_.Status -like "OK")) { $Matches[0] } }
 $regedit_path = "HKCU:\SOFTWARE\SimonTatham\PuTTY\Sessions\"
 $regedit_source = "HKCU:\SOFTWARE\SimonTatham\PuTTY\Sessions\Default%20Settings"
+$location = Get-location
 
 try {
-    foreach ($port in $ComPorts_notworking)
-    {
-        $noworkingport = $regedit_path + $port
-        Remove-Item -Path $noworkingport
-    }
-    
+	#Deleting old profiles of COM ports
+    set-location -path $regedit_path
+	$ComPorts_notworking= Get-ChildItem | Where-Object {$_.Name -match "COM*"} | select PSChildName
+	set-location -path $location
+	foreach ($port in $ComPorts_notworking.PSChildName)
+	{	
+		$port_path = $regedit_path + $port
+		remove-item $port_path
+	}
+	
+	#adding SSH Connection
+	$ComPorts_OK +='192.168.137.5'
+	
+	#Import default setting
     if(-Not(Test-Path $regedit_source))
     {
-        regedit.exe /C '.\default_settings.reg'
+		Write-Host "$regedit_source not found, importing registry"
+        regedit.exe /s /C '.\default_settings.reg'
+		start-sleep -s 3
     }
-    Foreach ( $port in $ComPorts_OK )
+	
+	#Configure ports 
+    foreach ($port in $ComPorts_OK)
     {
-        $registry = $regedit_path + $port.deviceID
-        if(-Not (Test-Path $registry))
-        {
-            Write-Host "Setting the port: '$($port."deviceID")'"
-            $registry = $regedit_path + $port.deviceID
-            Copy-Item -Path $regedit_source -Destination $registry
-            Set-ItemProperty -Path $registry -Name SerialLine -Value $port.deviceID
-            Set-ItemProperty -Path $registry -Name SerialSpeed -Value 115200
-        }
-    }
+        $registry = $regedit_path + $port
+		Write-Host "Setting the port: $port"
+		Copy-Item -Path $regedit_source -Destination $registry
+		if($port -like "192.168.137.5")
+		{
+			Set-ItemProperty -Path $registry -Name HostName -Value $port	
+		}
+		else
+		{
+			Set-ItemProperty -Path $registry -Name SerialLine -Value $port	
+			Set-ItemProperty -Path $registry -Name SerialSpeed -Value 115200
+		}
+		Write-Host "$port configured successfully"
+	}
 }
 catch{
     Write-Host "Something went wrong"
